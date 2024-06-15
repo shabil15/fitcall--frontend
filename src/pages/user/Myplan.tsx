fd2bf14e5de576737b6ae118e80c1c7a3f627735
shabil15 <mohammedshabil15@gmail.com>
2024-06-15 02:06:24 -0700

    fix: Confirmation modal before cancelling

--- src/pages/user/Myplan.tsx
+++ src/pages/user/Myplan.tsx
@@ -1,6 +1,6 @@
 import Navbar from "../../components/users/Navbar";
 import Footer from "../../components/users/Footer";
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../app/store";
 import { useNavigate } from "react-router-dom";
@@ -10,6 +10,11 @@ import { toast } from "react-toastify";
 import { MyError } from "../../validation/validationTypes";
 import { setCredential } from "../../slices/authSlice";
 import {useGetUserMutation} from "../../slices/userApiSlice";
+const public_stripe_key = 'pk_test_51PIo52SH9t7Bwd2YBOEZkvSjrfLfJwzyPXAAWLPuIqs3TWpaT4ErRl4DB8ReSZzGKu8tAdjJ96rU5YdkyDMyqc3M00Ppk33Q2p'
+import { usePaymentMutation } from '../../slices/userApiSlice';
+import { loadStripe,Stripe  } from "@stripe/stripe-js";
+import Swal from 'sweetalert2';
+
 
 function Myplan() {
   const { userInfo } = useSelector((state: RootState) => state.auth);
@@ -32,6 +37,46 @@ function Myplan() {
   const currentDate = new Date();
   const diff = startDate ? (currentDate.getTime() - startDates.getTime()) / (1000 * 3600 * 24) : null;
   const email = userInfo?.email;
+
+  const [payment] = usePaymentMutation();
+  const handlePurchase = async (amount: number) => {
+        
+    const result = await Swal.fire({
+      title: "Are you sure?",
+      text: "Commits you to a one-year plan with a dedicated trainer, video calls, customized nutrition plans, and premium support. Subscription upgrades immediately with prorated charges; cancellations may not refund partial months. ",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3BE48B",
+      cancelButtonColor: "#3d3636",
+      confirmButtonText: "Yes, upgrade it",
+      customClass: {
+        popup: "swal-custom-background",
+        title: "swal2-title",
+        content: "swal2-content",
+        confirmButton: "swal2-confirm",
+      },
+    });
+    if (result.isConfirmed) {
+    const stripePromise: Stripe | null  = await loadStripe(public_stripe_key);
+
+    const userId = userInfo?._id
+    const email = userInfo?.email
+
+    console.log("Stripe entered ");
+    const res = await payment({ amount,email,userId }).unwrap()
+    const session = res;
+    console.log(res)
+
+    if (stripePromise) {
+        stripePromise.redirectToCheckout({
+            sessionId: session.data
+        });
+    } else {
+        console.error('Failed to initialize Stripe');
+        // Handle the error appropriately
+    }
+  }
+}
   
   useEffect(() => {
     const fetchUser = async () => {
@@ -47,6 +92,23 @@ function Myplan() {
   }, [getUser, email, dispatch]);
 
   const handleCancelSubscription = async () => {
+  const result = await Swal.fire({
+    title: "Are you sure?",
+    text: "This action will cancel your subscription.",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3BE48B",
+    cancelButtonColor: "#3d3636",
+    confirmButtonText: "Yes, cancel it",
+    customClass: {
+      popup: "swal-custom-background",
+      title: "swal2-title",
+      content: "swal2-content",
+      confirmButton: "swal2-confirm",
+    },
+  });
+
+  if (result.isConfirmed) {
     try {
       const res = await cancelSubscription(userId).unwrap();
       dispatch(setCredential({ ...res.user }));
@@ -55,10 +117,11 @@ function Myplan() {
     } catch (error) {
       console.error("Error cancelling subscription:", error);
       toast.error(
-        (error as MyError)?.data?.message || (error as MyError)?.error
+        (error as MyError)?.data?.message || (error as MyError)?.error || "An error occurred."
       );
     }
-  };
+  }
+};
   return (
     <div className="bg-secondary">
       <Navbar />
@@ -99,6 +162,7 @@ function Myplan() {
 
             {userInfo.subscriptions[userInfo.subscriptions.length - 1].plan ===
             "Monthly" ? (
+              <div>
               <div className="space-y-8 lg:grid lg:grid-cols-1 sm:gap-6 xl:gap-10 lg:space-y-0">
                 <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-white backdrop-filter backdrop-blur-sm bg-opacity-10 border border-white-100 rounded-lg shadow dark:border-white-600 xl:p-8">
                   <h3 className="mb-4 text-2xl font-semibold">MONTHLY PLAN</h3>
@@ -207,6 +271,15 @@ function Myplan() {
                   )}
                 </div>
               </div>
+                  <div className="flex justify-center relative">
+                  <button
+                    onClick={()=>handlePurchase(36000)} 
+                    className="mt-4 bg-primary text-secondary hover:bg-green-700 hover:text-white font-bold transition-colors py-2 px-4 rounded-lg"
+                  >
+                    Upgrade to Annual Subscription
+                  </button>
+                  </div>
+              </div>
             ) : (
               <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-white backdrop-filter backdrop-blur-sm bg-opacity-10 border border-white-100 rounded-lg  shadow xl:p-8">
                 <h3 className="mb-4 text-2xl font-semibold">ANNUAL PLAN</h3>
@@ -296,26 +369,51 @@ function Myplan() {
                     <span>Premium support (chat and email)</span>
                   </li>
                 </ul>
-                {/* <button onClick={()=>handlePurchase(39999)} className="bg-primary text-secondary hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Get started</button> */}
-                <p>
-                  You can cancel your subscription within the first 3 days after
-                  the start date.
-                </p>
+                {diff !== null && diff < 3 ? (
+                  
+                    <div>
+                    <p>
+                      You can cancel your subscription within the first 3 days
+                      after the start date.
+                    </p>
+                  
+                  <button
+                    onClick={handleCancelSubscription}
+                    className="mt-4 bg-primary text-secondary hover:bg-green-700 hover:text-white font-bold transition-colors py-2 px-4 rounded-lg"
+                  >
+                    Cancel Subscription
+                  </button>
+                  </div>
+                  ) : (
+                    ""
+                  )}
+                
               </div>
             )}
           </div>
         ) : (
-          <div className="relative h-svh flex justify-center  items-center text-center">
-            <h1 className="text-4xl tracking-tight font-extrabold font-customFont text-white ">
-              You have no Subscription plan
-            </h1>
-            <button
-              onClick={() => navigate("/pricing")}
-              className="bg-primary font-bold text-secondary rounded-lg p-4 mx-4"
-            >
+          // <div className="relative h-svh flex justify-center  items-center text-center">
+          //   <h1 className="text-4xl tracking-tight font-extrabold font-customFont text-white ">
+          //     You have no Subscription plan
+          //   </h1>
+          //   <button
+          //     onClick={() => navigate("/pricing")}
+          //     className="bg-primary font-bold text-secondary rounded-lg p-4 mx-4"
+          //   >
+          //     TAKE A PLAN{" "}
+          //   </button>
+          // </div>
+          <div className="flex justify-center relative items-center mt-10">
+    <img src="../../../src/assets/9264822.png" alt="No Subscriptions" className="w-full max-w-md" />
+    < h1 className="text-white text-2xl ">You have no Subscription plan</h1>
+    <button
+            onClick={() => navigate("/pricing")}
+             className="bg-primary font-bold text-secondary rounded-lg p-4 mx-4"
+             >
               TAKE A PLAN{" "}
             </button>
-          </div>
+
+  </div>
         )}
       </section>
       :
