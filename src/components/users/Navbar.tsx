059fbb486f9d73f7cdbdb22bdf6d89b24802bcb6
shabil15 <mohammedshabil15@gmail.com>
2024-07-02 04:43:42 -0700

    update: navbar in user side made responsive and offcanvas added

--- src/components/users/Navbar.tsx
+++ src/components/users/Navbar.tsx
@@ -1,10 +1,8 @@
-
-import { Fragment, useEffect, useState } from 'react';
-import { Menu, Transition } from '@headlessui/react';
+import { Fragment, useEffect, useState, useRef } from 'react';
 import { useDispatch, useSelector } from "react-redux";
+import { Menu, Transition } from '@headlessui/react';
 import { Link, useNavigate } from 'react-router-dom';
 import Swal from "sweetalert2";
-import { openLoginModal } from "../../slices/modalSlices/loginModal";
 import Login from "../../components/common/login";
 import { RootState } from "../../app/store";
 import { userLogout } from "../../slices/authSlice";
@@ -14,7 +12,6 @@ import { ButtonsCard } from '../ui/ButtonCards';
 import { FaRegBell } from "react-icons/fa";
 import Notifications from './Notifications';
 
-
 export default function Navbar() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -23,7 +20,10 @@ export default function Navbar() {
 
   const [prevScrollPos, setPrevScrollPos] = useState(0);
   const [visible, setVisible] = useState(true);
-  const [showNotifications, setShowNotifications] = useState(false); 
+  const [showNotifications, setShowNotifications] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // State for off-canvas menu
+
+  const menuRef = useRef(null); // Ref for off-canvas menu
 
   useEffect(() => {
     const handleScroll = () => {
@@ -37,9 +37,22 @@ export default function Navbar() {
     return () => window.removeEventListener("scroll", handleScroll);
   }, [prevScrollPos, visible]);
 
-  function classNames(...classes: string[]) {
-    return classes.filter(Boolean).join(' ');
-  }
+  useEffect(() => {
+    // Function to close menu when clicking outside
+    function handleClickOutside(event) {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setIsMenuOpen(false);
+      }
+    }
+
+    // Bind the event listener
+    document.addEventListener("mousedown", handleClickOutside);
+
+    // Clean up the event listener on unmount
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   const handleLoginButtonClick = () => {
     dispatch(openLoginModal());
@@ -70,13 +83,14 @@ export default function Navbar() {
         toast.success(res.message);
       } catch (error) {
         console.error(error);
+        toast.error("Failed to logout");
       }
     }
   };
 
   return (
-    <div className={`fixed top-0 w-full z-20 transition-transform duration-300 ${visible ? 'translate-y-0' : '-translate-y-full'}`}>
-      <nav className="max-w-screen-xl flex items-center justify-between mx-auto p-4  text-white">
+    <div className={`fixed top-0 right-0 w-full z-20 transition-transform duration-300 ${visible ? 'translate-y-0' : '-translate-y-full'}`}>
+      <nav className="max-w-screen-xl flex items-center justify-between mx-auto p-4 text-white">
         <a href="/" className="flex items-center">
           <img
             src="../../../src/assets/FITcALL lOGO.png"
@@ -87,6 +101,54 @@ export default function Navbar() {
             FitCall
           </span>
         </a>
+        
+        {/* Off-canvas menu on the right */}
+        <div ref={menuRef} className={`fixed inset-y-0 right-0 w-64 shadow-lg rounded-lg z-20 py-6 px-4 transform ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-300 `}>
+          <ul className="space-y-4 bg-secondary rounded-xl">
+            <li>
+              <Link
+                to={"/"}
+                className="block py-2 px-3 text-white rounded hover:bg-gray-900"
+                aria-current="page"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                to={"/trainers"}
+                className="block py-2 px-3 text-white rounded hover:bg-gray-900"
+              >
+                Trainers
+              </Link>
+            </li>
+            <li>
+              <Link
+                to={"/pricing"}
+                className="block py-2 px-3 text-white rounded hover:bg-gray-900"
+              >
+                Pricing
+              </Link>
+            </li>
+            <li>
+              <Link
+                to={"/aboutus"}
+                className="block py-2 px-3 text-white rounded hover:bg-gray-900"
+              >
+                About us
+              </Link>
+            </li>
+            <li>
+              <Link
+                to={"/contactus"}
+                className="block py-2 px-3 text-white rounded hover:bg-gray-900"
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+
         <div className="items-center hidden w-full md:flex md:w-auto">
           <ul className="flex flex-col md:flex-row md:space-x-8">
             <li>
@@ -101,7 +163,7 @@ export default function Navbar() {
             <li>
               <Link
                 to={"/trainers"}
-                className="block py-2 px-3  rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary"
+                className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary"
               >
                 Trainers
               </Link>
@@ -109,7 +171,7 @@ export default function Navbar() {
             <li>
               <Link
                 to={"/pricing"}
-                className="block py-2 px-3  rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary"
+                className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary"
               >
                 Pricing
               </Link>
@@ -117,7 +179,7 @@ export default function Navbar() {
             <li>
               <Link
                 to={"/aboutus"}
-                className="block py-2 px-3  rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary"
+                className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary"
               >
                 About us
               </Link>
@@ -125,26 +187,26 @@ export default function Navbar() {
             <li>
               <Link
                 to={"/contactus"}
-                className="block py-2 px-3  rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary"
+                className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary"
               >
                 Contact
               </Link>
             </li>
           </ul>
         </div>
-        
+
+        {/* Hamburger menu button for mobile */}
         
 
         <div className="flex items-center space-x-3">
-        {userInfo?(
-          <div className="relative">
-          <FaRegBell className='cursor-pointer mr-3' onClick={() => setShowNotifications(!showNotifications)} />
-          {showNotifications && <Notifications userId={userInfo._id} />}
-        </div>
-        ):""}
           {userInfo ? (
-          <div className="">
-            
+            <div className="relative">
+              <FaRegBell className='cursor-pointer mr-3' onClick={() => setShowNotifications(!showNotifications)} />
+              {showNotifications && <Notifications userId={userInfo._id} />}
+            </div>
+          ) : ""}
+          {userInfo ? (
+            <div className="flex">
               <Menu as="div" className="relative">
                 <div>
                   <Menu.Button className="relative flex items-center justify-center w-8 h-8 rounded-full bg-primary text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
@@ -174,22 +236,22 @@ export default function Navbar() {
                   <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-secondary py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                     <Menu.Item>
                       {({ active }) => (
-                        <a
-                          onClick={() => navigate("/profile")}
-                          className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-white hover:bg-primary hover:text-secondary')}
+                        <Link
+                          to={"/profile"}
+                          className={`block px-4 py-2 text-sm text-white hover:bg-gray-700 `}
                         >
                           Your Profile
-                        </a>
+                        </Link>
                       )}
                     </Menu.Item>
                     <Menu.Item>
                       {({ active }) => (
-                        <a
-                          onClick={() => navigate("/myplan")}
-                          className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-white hover:bg-primary hover:text-secondary')}
+                        <Link
+                          to={"/myplan"}
+                          className={`block px-4 py-2 text-sm text-white  hover:bg-gray-700 hover:text-secondary'}`}
                         >
                           My Plan
-                        </a>
+                        </Link>
                       )}
                     </Menu.Item>
                     <Menu.Item>
@@ -197,7 +259,7 @@ export default function Navbar() {
                         <a
                           onClick={handleLogout}
                           href="#"
-                          className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-white hover:bg-primary hover:text-secondary')}
+                          className={`block px-4 py-2 text-sm text-white hover:bg-gray-700 hover:text-secondary'}`}
                         >
                           Sign out
                         </a>
@@ -206,37 +268,46 @@ export default function Navbar() {
                   </Menu.Items>
                 </Transition>
               </Menu>
-          </div>
-          ) : (
-            <ButtonsCard >
-<button
-             onClick={handleLoginButtonClick}
-             className="inline-flex h-12 animate-shimmer bg-primary items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-white transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"  
-           >
-Get started
-</button>
-</ButtonsCard>
-          )}
+              <div className="md:hidden">
           <button
-            data-collapse-toggle="navbar-sticky"
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
             type="button"
-            className="inline-flex items-center justify-center w-10 h-10 p-2 text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 text-gray-400"
-            aria-controls="navbar-sticky"
-            aria-expanded="false"
+            className="inline-flex items-center justify-center w-10 h-10 p-2 text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+            aria-controls="mobile-menu"
+            aria-expanded={isMenuOpen}
           >
             <span className="sr-only">Open main menu</span>
             <svg
-              className="w-5 h-5"
+              className={`${isMenuOpen ? 'hidden' : 'block'} w-6 h-6`}
               xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 17 14"
+              fill="none"
+              viewBox="0 0 24 24"
               stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
             >
-              <path d="M1 1h15M1 7h15M1 13h15" />
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
+            </svg>
+            <svg
+              className={`${isMenuOpen ? 'block' : 'hidden'} w-6 h-6`}
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
             </svg>
           </button>
+        </div>
+            </div>
+          ) : (
+            <ButtonsCard>
+              <button
+                onClick={handleLoginButtonClick}
+                className="inline-flex h-12 animate-shimmer bg-primary items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-white transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+              >
+                Get started
+              </button>
+            </ButtonsCard>
+          )}
         </div>
       </nav>
       <Login />
