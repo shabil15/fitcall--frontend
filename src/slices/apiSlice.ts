cf22786e4f74e2a2899dbb4a6833fa8bbfdb10bb
shabil15 <mohammedshabil15@gmail.com>
2024-07-19 03:44:26 -0700

    update: api slice updated

--- src/slices/apiSlice.ts
+++ src/slices/apiSlice.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseQuery = fetchBaseQuery({ baseUrl: "" });
+const baseQuery = fetchBaseQuery({ baseUrl:`${import.meta.env.VITE_BASE_URL}`});
 
 export const apiSlice = createApi({
   baseQuery,
